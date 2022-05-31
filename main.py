a583fa998c87e2f5d19d3380300fcc333342765d
Bernard Pratama Wongso <56827357+bernardpwn@users.noreply.github.com>
2022-05-31 13:44:28 +0800

    Update

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-from google.api_core.client_options import ClientOptions
-from googleapiclient import discovery
-
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello world"
-@app.route('/predict',methods=['POST'])
-def predict():
-    userid = request.form.get('userid')
-    endpoint = 'https://asia-southeast1-ml.googleapis.com/'
-    client_options = ClientOptions(api_endpoint=endpoint)
-    ml = discovery.build('ml', 'v1', client_options=client_options)
 
-    request_body = { 'instances': [{"input_1": "32"}] }
-    request = ml.projects().predict(
-        name='projects/delta-essence-349912/models/cobamodel/versions/versi2',
-        body=request_body)
-
-    response = request.execute()
-    print(response)
 if __name__ == '__main__':
     app.run(debug=True)
